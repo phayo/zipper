cebccbb60f2cd10f39ed42775ea96d5335894c09
phayo <anazodogodwin@gmail.com>
2019-11-22 19:04:07 +0100

    Add FileHandler.java and runner code

--- src/zip/FileHandler.java
+++ src/zip/FileHandler.java
@@ -1,5 +1,39 @@
 package zip;
 
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FileHandler {
-    
+    /**
+     * Method for creating a zip file on the specified path
+     * @param zipPath generated path where the zip is to be stored
+     * @return {@code Filesystem}
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    public FileSystem createZip(Path zipPath) throws IOException, URISyntaxException {
+        Map<String, String> providerProps = new HashMap<String, String>() {{
+            put("create", "true");
+        }};
+        URI zipURI = new URI("jar:file", zipPath.toUri().getPath(), null);
+        FileSystem fs = FileSystems.newFileSystem(zipURI, providerProps);
+        return fs;
+    }
+
+    /**
+     * Method for copying one file into the zip
+     * @param zipFs
+     * @param filename
+     * @throws IOException
+     */
+    public static void copyToZip(FileSystem zipFs, String filename) throws IOException {
+        Path sourceFile = Paths.get(filename);
+        Path destFile = zipFs.getPath("/"+filename);
+        Files.copy(sourceFile, destFile, StandardCopyOption.REPLACE_EXISTING);
+    }
+
 }
--- src/zip/Main.java
+++ src/zip/Main.java
@@ -1,12 +1,24 @@
 package zip;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileReader;
-import java.io.InputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.Paths;
 import java.nio.file.Path;
+
 
 public class Main {
     public static void main(String[] args) {
-
+        String name = "data.zip";
+        FileHandler fh = new FileHandler();
+        try (FileSystem fs = fh.createZip(Paths.get(name))) {
+            for (String f : args) {
+                FileHandler.copyToZip(fs, f.strip());
+                System.out.println("added '" + f + "' to the archive!");
+            }
+            System.out.println("Your archive is ready : " + name);
+            Path p = Paths.get(fs.toString());
+            System.out.println(p.toUri().toString());
+        } catch(Exception e) {
+            System.out.println("Exception : " + e.getMessage());
+        }
     }
 }
